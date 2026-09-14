42efcca7c35f10750fc208180c2e1f84cd4d4c80
Арина Севрюк <politgoo@gmail.com>
2026-09-14 23:53:30 +0400

    sync

--- magic_vkr_lastrun.py
+++ magic_vkr_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2025.1.1),
-    on Fri Sep 11 22:35:50 2026
+    on Mon Sep 14 23:41:24 2026
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -64,7 +64,7 @@ or run the experiment with `--pilot` as an argument. To change what pilot
 PILOTING = core.setPilotModeFromArgs()
 # start off with values from experiment settings
 _fullScr = True
-_winSize = (1024, 768)
+_winSize = [1710, 1107]
 # if in pilot mode, apply overrides according to preferences
 if PILOTING:
     # force windowed mode
